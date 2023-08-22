9d6c59dfec634e8c69ac9b64c0527e0bbef9a380
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-08-22 08:24:30 -0300

    Metodo POST no ProductController

--- firstproject/src/main/java/com/maven/firstproject/controllers/ProductController.java
+++ firstproject/src/main/java/com/maven/firstproject/controllers/ProductController.java
@@ -1,7 +1,27 @@
 package com.maven.firstproject.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.maven.firstproject.dtos.ProductRecordDto;
+import com.maven.firstproject.models.ProductModel;
+import com.maven.firstproject.repositories.ProductRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class ProductController {
 
+    @Autowired
+    ProductRepository productRepository;
+
+    @PostMapping("/products")
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto) {
+        var productModel = new ProductModel();
+        BeanUtils.copyProperties(productRecordDto, productModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+    }
 }
