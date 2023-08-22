4f99378328ee1da053635956e0e56db4d152cb58
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-08-22 07:24:41 -0300

    Iniciando o projeto

--- firstproject/src/main/java/com/maven/firstproject/FirstprojectApplication.java
+++ firstproject/src/main/java/com/maven/firstproject/FirstprojectApplication.java
@@ -1,13 +1,17 @@
 package com.maven.firstproject;
 
+import com.maven.firstproject.controllers.ProductController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class FirstprojectApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(FirstprojectApplication.class, args);
+
 	}
 
 }
