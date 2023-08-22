33441a1a80a81806750144bf93cdae575b24d1e5
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-08-22 07:32:16 -0300

    Criando o package repositories

---
+++ firstproject/src/main/java/com/maven/firstproject/repositories/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.maven.firstproject.repositories;
+
+import com.maven.firstproject.models.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+
+}
